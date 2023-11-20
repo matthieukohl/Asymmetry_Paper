d7c41890db44bc63fa80723c0a40ab1b3817dff5
matthieukohl <98239886+matthieukohl@users.noreply.github.com>
2023-11-20 16:45:19 -0500

    replace

--- inversion/root_1d_toy_model.m
+++
@@ -1,7 +0,0 @@
-function [F] = root_1d_toy_model(x,kd,r)
-%1d toy model: x = k_u
-
-F = x-kd*(1+kd^2)./(1+x^2*r);
-
-end
-
--- inversion/root_1d_toy_model_refined.m
+++
@@ -1,9 +0,0 @@
-function [F] = root_1d_toy_model_refined(x,r,k)
-
-% solve the nonlinear constraint for x=ku
-
-kd = k*x/(2*x-k);
-
-F = x-kd*(1+kd^2)./(1+x^2*r);
-
-end
